c249663b294112136257444fcbd316b2f92de021
Mynewgi <mina.m.monsef.apr@gmail.com>
2022-12-09 09:14:03 -0500

    first commit

---
+++ src/test/java/automation_code_assignment_9/PNC.java
@@ -0,0 +1,19 @@
+package automation_code_assignment_9;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class PNC {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.pnc.com/en/personal-banking.html");
+
+	}
+
+}
